d6fa434ecef6a24d86192cb30eaea27e3ad2bff6
Vinicius Santos <vinobeacoup56@gmail.com>
2026-03-05 11:35:17 -0300

    Add StandardResponse model and update UserTransactions
    Added StandardResponse model and updated UserTransactions with model_config.

--- schema.py
+++ schema.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from decimal import Decimal
+from typing import Optional
 
 class UserCreate(BaseModel):
     full_name: str
@@ -10,4 +11,11 @@ class UserCreate(BaseModel):
 class UserTransactions(BaseModel):
     destiny_cpf: str
     quantity: Decimal
-
+    model_config = {
+        "from_attributes": True
+    }
+class StandardResponse(BaseModel):
+    success: bool
+    message: str
+    data: Optional[dict] = None
+    
