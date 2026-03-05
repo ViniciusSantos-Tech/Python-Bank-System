040ba5c862ee0f8ace0a3e310eddf5d005a0bef7
Vinicius Santos <vinobeacoup56@gmail.com>
2026-03-05 11:34:22 -0300

    Add Transactions class with foreign key to Account
    Added Transactions class for transaction history management.

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, String, Integer, Column, Numeric
+from sqlalchemy import create_engine, String, Integer, Column, Numeric, TIMESTAMP, func, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 from dotenv import load_dotenv
@@ -10,6 +10,7 @@ url_bank = os.getenv("DATABASE_URL")
 
 class Account(Base):
     __tablename__ = "BankAccounts"
+    __table_args__ = {"schema": "Bank"}
 
     id =  Column(Integer, primary_key=True, autoincrement=True, index=True)
     full_name = Column(String)
@@ -18,6 +19,18 @@ class Account(Base):
     username = Column(String, unique=True)
     hashed_password = Column(String)
     balance = Column(Numeric)
+
+class Transactions(Base):
+    __tablename__ = "History"
+    __table_args__ = {"schema": "Bank"}
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    account_id = Column(Integer, ForeignKey("Bank.BankAccounts.id", ondelete="CASCADE"), nullable=False)
+    type = Column(String, nullable=False)
+    amount = Column(Numeric(10,2), nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+    description = Column(String)
+
     
 engine = create_engine(url_bank)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
